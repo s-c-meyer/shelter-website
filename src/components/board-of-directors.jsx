b4b222ebae0c04dd3dbc3c13e2f436c93c1eb956
s-c-meyer <meyer.samuelc@gmail.com>
2024-05-29 14:01:26 -0500

    Updated Board of Directors Page
    Updated the board of directors page with reusable director components that take a director object as a prop with a name, short description, and picture.

--- src/components/board-of-directors.jsx
+++ src/components/board-of-directors.jsx
@@ -1,7 +1,26 @@
+import DirectorBio from "./director-bio";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 export default function Board () {
+  let director = {
+    name: "Example Name",
+    description: "This is an example description of one of the board members, possibly with an official title, or could also include a short description of the work they do for the organization"
+  }
+
   return (
     <>
-      <p>This is the Board of Directors Page</p>
+      <Container className="d-flex justify-content-center">
+        <Row className="d-flex justify-content-center">
+          <Col xs={12} sm={11}>
+            <DirectorBio director={director} />
+            <DirectorBio director={director} />
+            <DirectorBio director={director} />
+            <DirectorBio director={director} />
+          </Col>
+        </Row>
+      </Container>
     </>
   )
 }
---
+++ src/components/director-bio.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/prop-types */
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import Stack from "react-bootstrap/Stack";
+
+export default function DirectorBio ({ director }) {
+  return (
+    <Container className="director-bio-card mt-5">
+      <Row className="d-flex">
+        <Col md={4}>
+          <Image src="https://placehold.co/600x400" alt="placeholder image" width="100%"/>
+        </Col>
+        <Col className="d-flex" md={8}>
+          <Stack className="align-items-center justify-content-center" gap={3}>
+            <div className="barlow-bold director-name">{director.name}</div>
+            <div className="barlow-light">{director.description}</div>
+          </Stack>
+        </Col>
+        <hr className="border-line" />
+
+      </Row>
+    </Container>
+  )
+}
+
